c54f880f221a662288618951f0a1c44f91aebd24
konstantin <konstantin@seditionart.com>
2012-10-16 16:23:04 +0100

    custom event emitter mechanism to replace the jQuery-based one, smarter extend mechanism and some bug fixes

--- dassie_mvc.js
+++ dassie_mvc.js
@@ -7,75 +7,99 @@ dassie.models = {};
 dassie.controllers = {};
 
 (function($){
-  // TODO: build in route support
+  var global = this;
+
+  var eventEmitter = {
+    initEventEmitter: function(){
+      this._eH = {}; //events handler
+    },
+    bind: function(event, callback){
+      if(this._eH[event] === undefined){
+        this._eH[event] = [];
+      }
+      this._eH[event].push(callback);
+    },
+    trigger: function(event,data){
+      if(this._eH[event] !== undefined){
+        for(i=0, l = this._eH[event].length; i < l; i++){
+          this._eH[event][i](data);
+        }
+      }
+    }
+  };
+
   dassie.Model = function(){
+    this.initEventEmitter();
     this._data = {};
-    this._$o = $("<div>"); // empty jQuery object to use for event emitting
     this.construct.apply(this,arguments);
   }
-  $.extend(dassie.Model.prototype,{
-    bind: function(event, callback){
-      this._$o.bind(event,callback);
-    },
-    trigger: function(event, data){
-      this._$o.trigger(event, data);
-    },
-    clear: function(){
-      this._data = {};
-      this.trigger("clear",this._data);
-    },
-    setData: function(data){
-      if(typeof data === "object"){
-        for(prop in data){
-          if(data.hasOwnProperty(prop)){
-            this._data[prop] = data[prop];
+  $.extend(
+    dassie.Model.prototype,
+    eventEmitter,
+    {
+      clear: function(){
+        this._data = {};
+        this.trigger("clear",this._data);
+      },
+      setData: function(data){
+        if(typeof data === "object"){
+          for(prop in data){
+            if(data.hasOwnProperty(prop)){
+              this._data[prop] = data[prop];
+            }
           }
         }
-      }
 
-      this.trigger("setData",data);
-    },
+        this.trigger("setData",data);
+      },
 
-    getData: function(opt){
-      if(typeof opt === "undefined") return this._data;
+      getData: function(opt){
+        if(typeof opt === "undefined") return this._data;
 
-      if(typeof opt === "object"){
-        var prop_subset = {};
-        
-        for(prop in opt){
-          if(opt.hasOwnProperty(prop)){
-            prop_subset[prop] = this._data[prop];
+        if(typeof opt === "string"){
+          return this._data[opt]
+        }
+
+        if(typeof opt === "object"){
+          if(opt instanceof Array){
+            var data_subset = {};
+            
+            for(i=0,l=opt.length;i<l;i++){
+              data_subset[opt[i]] = this._data[opt[i]];
+            }
+            return data_subset;
           }
         }
-        return prop_subset;
+
+      },
+      
+      save: function(data,callback){
+        //TODO
+      },
+
+      reload: function(data,callback){
+        //TODO
       }
+    });
 
-      if(typeof opt === "string"){
-        return this._data[opt]
-      }
-    },
-    
-    save: function(data,callback){
-      //TODO
-    },
 
-    reload: function(data,callback){
-      //TODO
-    }
-  });
-
+  //REFACTOR
   dassie.Controller = function(){
     if(typeof this.preload === 'object' && !$.isEmptyObject(this.preload)){
       // asynchronous preloading:
       for(prop in this.preload){
         if(this.preload.hasOwnProperty(prop)){
+          var loaded = false; //makes sure that onReady would be called only once
+
           (function(p){
             var loading_model = this.preload[p]();
 
             //callback, for when it's loaded:
             loading_model.bind("setData",function(){
               this[p] = loading_model;
-              onReady.apply(this,arguments); 
+              if(!loaded){
+                loaded = onReady.apply(this,arguments); 
+              }
             }.bind(this));
           }).call(this,prop);
         }
@@ -89,35 +113,51 @@ dassie.controllers = {};
     if(typeof this.preload === "object"){
       //check if we preloaded everything we wanted to
       for(prop in this.preload){
+
         if(this.preload.hasOwnProperty(prop) && !this.hasOwnProperty(prop)){
-          return;
+          return false;
         }
       }
       this.preload = undefined;
     }
+
     this.construct.apply(this,arguments);
+    return true;
   }
 
-  dassie.View = function(opt){
-    this.$el = opt.$el;
-    this._$o = $(""); // empty jQuery object to use for event emitting
+  dassie.View = function(opts){
+    if(opts === undefined) opts = {};
+    $.extend(this, opts);
+    this.initEventEmitter();
     this.construct.apply(this,arguments);
+    //TODO (maybe, when dassie is cool enough :): this.delegateEvents();
   }
 
-  $.extend(dassie.View.prototype,{
-    bind: function(){
-      this._$o.bind(arguments);
-    },
-    trigger: function(){
-      this._$o.trigger(arguments);
-    }
-  });
+  $.extend(
+    dassie.View.prototype,
+    eventEmitter
+  );
 
+  //helper functions
 
   var extend =  function(props,static_props){
-    $.extend(this.prototype,props);
-    $.extend(this,static_props);
-    return this;
+    var parent = this;
+    var child;
+
+    child = function(){
+      parent.apply(this,arguments);
+    };
+
+    $.extend(child,parent,static_props)
+
+    // using surrogate object to set child's prototype chain to inherit from parent
+    var F = function(){ this.constructor = child; };
+    F.prototype = parent.prototype;
+    child.prototype = new F;
+
+    $.extend(child.prototype,props)
+    
+    return child;
   }
 
   dassie.Model.extend = dassie.Controller.extend = dassie.View.extend = extend;
