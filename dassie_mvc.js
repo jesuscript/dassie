c97ca798e8eb7416a8e52a029d6e024e2056b997
konstantin <konstantin@seditionart.com>
2012-10-17 20:24:12 +0100

    ajax helper functions on model

--- dassie_mvc.js
+++ dassie_mvc.js
@@ -1,6 +1,8 @@
 // why dassie? cuz they're soooo cute!!! ^_^
 // http://www.youtube.com/watch?v=-svEVVyCM-0&feature=related
 
+//TODO (maybe): delegate binding on collections of views (collections of views? o_O )
+
 dassie = {};
 dassie.views = {};
 dassie.models = {};
@@ -35,7 +37,8 @@ dassie.controllers = {};
     }else{
       this._data = {};
     }
-    this.construct.apply(this,arguments);
+
+    if(this.construct !== undefined) this.construct.apply(this,arguments);
   }
   $.extend(
     dassie.Model.prototype,
@@ -84,18 +87,83 @@ dassie.controllers = {};
       reload: function(data,callback){
         //TODO
       }
-    });
+    }
+  );
+  //static methods:
+  $.extend(dassie.Model,{
+    //TODO: add error callbacks too?
+    _get: function(url,data,callback){
+      var args = getAjaxArguments(arguments);
+      $.ajax({
+        type: "GET",
+        url: url,
+        data: $.extend({format: "json", _method: "GET"}, args.data),
+        success: args.callback
+      });
+    },
+    _post: function(path,data,callback){
+      var args = getAjaxArguments(arguments);
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: $.extend({format: "json", _method: "POST"}, args.data),
+        success: args.callback
+      });
+    },
+    _put: function(path,data,callback){
+      var args = getAjaxArguments(arguments);
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: $.extend({format: "json", _method: "PUT"}, args.data),
+        success: args.callback
+      });
+    },
+    _delete: function(path,callback){
+      var args = getAjaxArguments(arguments);
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: $.extend({format: "json", _method: "DELETE"}, args.data),
+        success: args.callback
+      });
+    }
+  });
 
+  function getAjaxArguments(args){
+    var url, data, callback;
+    url = args[0];
+
+    if(args.length == 2){
+      if(typeof args[1] === "object"){
+        data = args[1];
+      }else if(typeof args[1] === "function"){
+        callback = args[1]
+      }else{
+        throw new Error("getAjaxArguments(): 2nd argument must be an object or a function");
+      }
+    }else{
+      if(typeof args[1] !== "object"){
+        throw new Error("getAjaxArguments(): 2nd argument must be an object");
+      }
+      if(typeof args[2] !== "function"){
+        throw new Error("getAjaxArguments(): 3rd argument must be a function");
+      }
+      data = args[1];
+      callback = args[2];
+    }
+    return {url: url, data: data, callback: callback};
+  }
 
   dassie.Controller = function(){
-    this.construct.apply(this,arguments);
+    if(this.construct !== undefined) this.construct.apply(this,arguments);
   }
 
   dassie.View = function(opts){
     if(opts === undefined) opts = {};
     $.extend(this, opts);
     this.initEventEmitter();
-    this.construct.apply(this,arguments);
+    if(this.construct !== undefined) this.construct.apply(this,arguments);
     //TODO (maybe, when dassie is cool enough :): this.delegateEvents();
   }
 
