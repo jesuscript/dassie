d542f47f564dfd58390d3ff1e9bc392025530ca6
konstantin <konstantin@seditionart.com>
2012-10-24 10:05:03 +0100

    model synchronisation functions now don't pass any arguments to the path functions

--- dassie_mvc.js
+++ dassie_mvc.js
@@ -1,7 +1,7 @@
 // why dassie? cuz they're soooo cute!!! ^_^
 // http://www.youtube.com/watch?v=-svEVVyCM-0&feature=related
 
-//TODO: delegate events
+//TODO: delegate events (e.g. view collections?);
 //TODO: think about how to load all locales from the server (not in this file tho)
 
 dassie = {};
@@ -30,6 +30,47 @@ dassie.controllers = {};
       }
     }
   };
+
+  var ajax_helpers = {
+    //TODO: add error callbacks too?
+    _get: function(){
+      var args = getAjaxArguments(arguments);
+
+      $.ajax({
+        type: "GET",
+        url: args.url,
+        data: $.extend({format: "json", _method: "GET"}, args.data),
+        success: args.callback
+      });
+    },
+    _post: function(){
+      var args = getAjaxArguments(arguments);
+      $.ajax({
+        type: "POST",
+        url: args.url,
+        data: $.extend({format: "json", _method: "POST"}, args.data),
+        success: args.callback
+      });
+    },
+    _put: function(){
+      var args = getAjaxArguments(arguments);
+      $.ajax({
+        type: "POST",
+        url: args.url,
+        data: $.extend({format: "json", _method: "PUT"}, args.data),
+        success: args.callback
+      });
+    },
+    _delete: function(){
+      var args = getAjaxArguments(arguments);
+      $.ajax({
+        type: "POST",
+        url: args.url,
+        data: $.extend({format: "json", _method: "DELETE"}, args.data),
+        success: args.callback
+      });
+    }
+  }
 
   dassie.Model = function(data){
     this.initEventEmitter();
@@ -78,64 +119,72 @@ dassie.controllers = {};
             return data_subset;
           }
         }
-
       },
-      
-      save: function(data,callback){
+      /* Sends a request to update the server-side representation of the model 
+       * and load model's properties to update the object's data
+       */
+
+      sync: function(data,callback){ 
+        if(typeof this.syncPath !== "function"){
+          throw new Error("sync():"+ typeof this.syncPath +"is not a function!");
+        }
+        var self = this;
+        
+        // asynchronous synchroniser lol
+        ajax_helpers._put(this.syncPath(), data, function(response){
+          $.extend(self.data,response);
+
+          self.trigger("update");
+          if(typeof callback === "function") callback(response);
+        });
+      },
+      /* works just like sync but wipes object's data before adding received properties to it*/
+      clean_sync: function(data,callback){
+        if(typeof this.syncPath !== "function"){
+          throw new Error("clean_sync(): "+ typeof this.syncPath +" is not a function!");
+        }
+        var self = this;
+        ajax_helpers._put(this.syncPath(), data, function(response){
+          delete self.data;
+          self.data = response;
+
+          self.trigger("update");
+          if(typeof callback === "function") callback(response);
+        });
+      },
+      /* Sends a request to get the model's properties, and updates the object's data */
+      load: function(data,callback){
+        var self = this;
+        if(typeof this.loadPath !== "function"){
+          throw new Error("load():" + typeof this.loadPath + " is not a function!");
+        }
+        ajax_helpers._get(this.loadPath(), data, function(response){
+          $.extend(self.data,response);
+
+          self.trigger("update");
+          if(typeof callback === "function") callback(response);
+        });
+      },
+      //TODO(maybe): reload() to wipe object's data and load it
+
+      /* Sends a request to create a new server-side representation of the model */
+      saveNew: function(data,callback){
         //TODO
       },
-
-      reload: function(data,callback){
+      /* Sends a request to destroy the server-side representation of the model */
+      destroy: function(data,callback){
         //TODO
       }
     }
   );
   //static methods:
-  $.extend(dassie.Model,{
-    //TODO: add error callbacks too?
-    _get: function(url,data,callback){
-      var args = getAjaxArguments(arguments);
-      $.ajax({
-        type: "GET",
-        url: url,
-        data: $.extend({format: "json", _method: "GET"}, args.data),
-        success: args.callback
-      });
-    },
-    _post: function(path,data,callback){
-      var args = getAjaxArguments(arguments);
-      $.ajax({
-        type: "POST",
-        url: url,
-        data: $.extend({format: "json", _method: "POST"}, args.data),
-        success: args.callback
-      });
-    },
-    _put: function(path,data,callback){
-      var args = getAjaxArguments(arguments);
-      $.ajax({
-        type: "POST",
-        url: url,
-        data: $.extend({format: "json", _method: "PUT"}, args.data),
-        success: args.callback
-      });
-    },
-    _delete: function(path,callback){
-      var args = getAjaxArguments(arguments);
-      $.ajax({
-        type: "POST",
-        url: url,
-        data: $.extend({format: "json", _method: "DELETE"}, args.data),
-        success: args.callback
-      });
-    }
-  });
+  $.extend(dassie.Model, ajax_helpers);
 
   function getAjaxArguments(args){
     var url, data, callback;
     url = args[0];
 
-    if(args.length == 2){
+    if(args.length == 2 && args[1] !== undefined){
       if(typeof args[1] === "object"){
         data = args[1];
       }else if(typeof args[1] === "function"){
@@ -144,11 +193,11 @@ dassie.controllers = {};
         throw new Error("getAjaxArguments(): 2nd argument must be an object or a function");
       }
     }else{
-      if(typeof args[1] !== "object"){
-        throw new Error("getAjaxArguments(): 2nd argument must be an object");
+      if(args[1] !== undefined && typeof args[1] !== "object"){
+        throw new Error("getAjaxArguments(): 2nd argument must be an object or undefined");
       }
-      if(typeof args[2] !== "function"){
-        throw new Error("getAjaxArguments(): 3rd argument must be a function");
+      if(args[2] !== undefined && typeof args[2] !== "function"){
+        throw new Error("getAjaxArguments(): 3rd argument must be a function or undefined");
       }
       data = args[1];
       callback = args[2];
@@ -189,7 +238,6 @@ dassie.controllers = {};
     var F = function(){ this.constructor = child; };
     F.prototype = parent.prototype;
     child.prototype = new F;
-
     $.extend(child.prototype,props)
     
     return child;
